ef973a67bb99d1488b55f1f2c4f5d9f01e4b618f
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-04 07:21:33 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//*****************************************************************************
+//** 2825. Make String a Subsequence Using Cyclic Increments    leetcode     **
+//*****************************************************************************
+
+bool canMakeSubsequence(char* str1, char* str2)
+{
+    int i = 0;
+    int n = strlen(str2); 
+
+    for (int j = 0; str1[j] != '\0'; ++j)
+    {
+        char c = str1[j];
+        char d = (c == 'z') ? 'a' : c + 1;
+
+        if (i < n && (str2[i] == c || str2[i] == d))
+        {
+            ++i;
+        }
+    }
+
+    return i == n;
+}
